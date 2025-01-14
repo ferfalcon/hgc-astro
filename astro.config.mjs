4fd83c30f6279a8dd0b8a45aa86a98d4dd72edde
Fer <fer.falcon.dg@gmail.com>
2025-01-14 10:12:41 -0300

    Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	base: '/hgc-astro/',
+	site: 'https://ferfalcon.github.io/hgc-astro/'
+});
